3fd6f92f962ac4b9e69372e3daf351d2bb6f54fa
Mikkili Sai Prasad Reddy <prasadreddymar6@gmail.com>
2025-05-09 23:12:17 +0530

    update: edited signup page

--- apps/web/src/app/(auth)/signup/page.tsx
+++ apps/web/src/app/(auth)/signup/page.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
-  )
-}
+    <div className="h-screen w-full flex items-center justify-center lg:justify-start">
+      <div className="w-full max-w-md lg:ml-[20%] px-6 lg:px-0"></div>
+    </div>
+  );
+};
 
-export default SignUp
+export default SignUp;
