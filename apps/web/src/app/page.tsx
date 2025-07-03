16b5d1e03545cd1385ecfe5e2957bbbff30774e0
Mikkili Sai Prasad Reddy <prasadreddymar6@gmail.com>
2025-07-03 22:06:38 +0530

    update: home page metadata added

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,6 +1,3 @@
-'use client';
-import * as React from 'react';
-import { useEffect, useState } from 'react';
 import Navbar from '@/components/homepage/navbar';
 import HeroImage from '@/components/homepage/heroImage';
 import HeroSection from '@/components/homepage/heroSection';
@@ -8,8 +5,39 @@ import Footer from '@/components/homepage/footer';
 import Section1 from '@/components/homepage/section';
 import { GridGradient } from '@dr/ui/components/base/grid-gradient';
 import { ThemeProvider } from '@/context/themeContext';
+import { Metadata } from 'next';
 
-export default function Home() {
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: "Digiresume",
+    description: "Build a dynamic digital portfolio and resume that stay in sync. Showcase your skills, projects, startups, and achievements.",
+    icons: {
+      icon: "/favicon.ico",
+    },
+    openGraph: {
+      title: "Digiresume",
+      description: "Build a dynamic digital portfolio and resume that stay in sync. Showcase your skills, projects, startups, and achievements.",
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}`,
+      images: [
+        {
+          url: "/ogimage.png",
+          width: 1200,
+          height: 630,
+          alt: "Digiresume's OpenGraph Image",
+        },
+      ],
+      siteName: "Digiresume",
+    },
+    twitter: {
+      card: "summary",
+      title: "Digiresume",
+      description: "Build a dynamic digital portfolio and resume that stay in sync. Showcase your skills, projects, startups, and achievements.",
+      images: ["/ogimage.png"],
+    },
+  };
+}
+
+export default async function Home() {
 
   return (
     <ThemeProvider>
